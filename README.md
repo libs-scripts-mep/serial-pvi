2a616a1bc38bbe8c12a9228bd111f04acb14dd9b
Lucas Kroth <lucas.kroth@inova.ind.br>
2023-11-14 12:07:45 -0300

    first commit v3.0

--- README.md
+++ README.md
@@ -17,3 +17,22 @@ Abra o terminal, e na pasta do script, rode:
 ```
 npm uninstall @libs-scripts-mep/serial-pvi
 ```
+
+## Atualizando
+
+Abra o terminal, e na pasta do script, rode:
+
+```
+npm update @libs-scripts-mep/serial-pvi
+```
+
+## Como utilizar
+
+Realize a importação:
+
+``` js
+import { SerialReqManager, SerialPVIUtil } from "../serial-pvi/serial-pvi.js"
+```
+
+As demais informações e instruções estarão disponíveis via `JSDocs`.
+
--- serial-pvi.js
+++ serial-pvi.js
@@ -1,72 +1,68 @@
-class SerialPVI {
+import Log from "../script-loader/utils-script.js"
+import FWLink from "../daq-fwlink/FWLink.js"
+
+export class SerialPVI {
    constructor(baud = 9600, parity = 0) {
       this.BAUD = baud
       this.PARITY = parity
       this.PORT = null
+      /** Configurações estéticas para log */
+      this.Log = {
+         /** Cor que será apresentadas as requisições no console */
+         req: Log.Colors.Blue.CornflowerBlue,
+         /** Cor que será apresentadas as respostas no console */
+         res: Log.Colors.Purple.Orchid,
+         /** Cor que será apresentadas os erros no console */
+         error: Log.Colors.Red.Crimson
+      }
    }
 
-   setPort(port) {
-      this.PORT = port
-   }
+   setPort(port) { this.PORT = port }
+   getPort() { return this.PORT }
+   isOpen(port = this.PORT) { return FWLink.runInstruction("serial.isopen", ['"' + port + '"']) == 1 }
+   close() { return FWLink.runInstruction("serial.close", ['"' + this.PORT + '"']) == 1 }
+   open() { return FWLink.runInstruction("serial.open", ['"' + this.PORT + '"', '"' + this.BAUD + '"', '"' + this.PARITY + '"']) == 1 }
 
-   getPort() {
-      return this.PORT
-   }
-
-   open() {
-      return pvi.runInstruction("serial.open", ['"' + this.PORT + '"', '"' + this.BAUD + '"', '"' + this.PARITY + '"']) == 1
-   }
-
-   close() {
-      return pvi.runInstruction("serial.close", ['"' + this.PORT + '"']) == 1
-   }
-
-   isOpen(port = this.PORT) {
-      return pvi.runInstruction("serial.isopen", ['"' + port + '"']) == 1
-   }
-
-   async ReadData(timeOut = 50) {
+   async ReadData(timeOut = 0) {
       return new Promise((resolve) => {
          setTimeout(() => {
-            const response = DAQ.runInstructionS("serial.readbytedata", [this.PORT])
-            console.log(`%c${this.PORT} <= ${response}`, 'color: #CCEE55')
+            const response = FWLink.runInstructionS("serial.readbytedata", [this.PORT])
+            Log.console(`${this.PORT} <= ${response}`, this.Log.res)
             resolve(response)
          }, timeOut)
       })
    }
 
    async SendBytes(data) {
-      console.log(`%c${this.PORT} => ${data}`, 'color: #00EE55')
-      return DAQ.runInstructionS("serial.sendbyte", [this.PORT, data]) == 1
-   }
-
-   async tryToOpen(port, timeout = 0) {
-      return new Promise((resolve) => {
-
-         const monitor = setInterval(() => {
-            if (!this.isOpen(port)) {
-
-               if (this.open(port)) {
-                  clearInterval(monitor)
-                  clearTimeout(timeOut)
-                  resolve(true)
-               }
-            }
-         }, 100)
-
-         const timeOut = setTimeout(() => {
-            clearInterval(monitor)
-            resolve(false)
-         }, timeout)
-      })
+      Log.console(`${this.PORT} <= ${data}`, this.Log.req)
+      return FWLink.runInstructionS("serial.sendbyte", [this.PORT, data]) == 1
    }
 
    /**
     * 
-    * @param {object} reqInfo { request: string, regex: RegExp, readTimeout: number, maxTries: number, tryNumber: number }
+    * @param {object} reqInfo
     * @returns 
+    * 
+    * # Exemplos
+    * 
+    * ```js
+    * const reqInfo = {
+    * request: String
+    * regex: RegExp
+    * readTimeout: Number //Opcional
+    * tryNumber: Number //Opcional
+    * maxTries: Number //Opcional
+    * }
+    * const serial = new SerialReqMenager(9600)
+    * const result = await serialInstance.portDiscover(reqInfo)
+    * ```
+    * ## Result
+    * 
+    * ```js
+    * {success: Boolean, port: String}
+    * ```
     */
-   async portDiscover(reqInfo, openTimeout = 1000) {
+   async portDiscover(reqInfo) {
 
       const portList = SerialPVIUtil.getPortList()
 
@@ -77,26 +73,26 @@ class SerialPVI {
             this.setPort(port)
             reqInfo.tryNumber = 1
 
-            if (await this.tryToOpen(port, openTimeout)) {
+            if (this.open()) {
 
-               const result = await this.reqResMatchBytes(reqInfo)
-               if (result.sucess) {
-                  return { sucess: true, port: this.getPort() }
+               const result = await this.WatchForResponse(reqInfo)
+               if (result.success) {
+                  return { success: true, port: this.getPort() }
                } else {
                   this.close()
                }
 
             } else {
-               console.log(`%c${port} - Impossivel abrir porta`, `color: #EE0066`)
+               Log.console(`${port} - Impossivel abrir porta`, this.Log.error)
             }
          }
 
          this.setPort(null)
-         return { sucess: false, port: this.getPort() }
+         return { success: false, port: this.getPort() }
 
       } else {
          this.setPort(null)
-         return { sucess: false, port: this.getPort() }
+         return { success: false, port: this.getPort() }
       }
    }
 
@@ -106,51 +102,61 @@ class SerialPVI {
     * @returns 
     */
    async reqResMatchBytes(reqInfo) {
-
       const { regex } = reqInfo
       const { request } = reqInfo
       const { maxTries } = reqInfo
       const { tryNumber } = reqInfo
       const { readTimeout } = reqInfo
 
-      return new Promise(async (resolve) => {
-
-         let response = null
-         let match = null
-
+      try {
          this.SendBytes(request)
-         response = await this.ReadData(readTimeout)
-         match = response.match(regex)
+         let response = await this.ReadData(readTimeout)
+         let match = response.match(regex)
 
          if (match) {
-            resolve({ sucess: true, response: match })
+            return { success: true, response: match }
          } else if (tryNumber < maxTries) {
             reqInfo.tryNumber++
-            resolve(this.reqResMatchBytes(reqInfo))
+            return this.reqResMatchBytes(reqInfo)
          } else {
-            resolve({ sucess: false, response: null })
+            return { success: false, response: null }
          }
-      })
+
+      } catch (error) {
+         console.error(error)
+         return { success: false, response: null }
+      }
    }
 }
 
-class SerialReqManager extends SerialPVI {
-
-   /**
-    * 
-    * @param {number} baud
-    * @param {number} parity Opcoes: 0, 1
-    * @param {string} policy Politica de gerenciamento, opcoes: "Queue" ou "Stack"
-    */
+/**
+ * 
+ * @param {number} baud 
+ * @param {number} parity 
+ * @param {string} policy Politica de gerenciamento 
+ * 
+ * # Exemplos
+ * 
+ * ```js
+ * const serial = new SerialReqManager(9600, 0)
+ * const serial = new SerialReqManager(9600, 0, "Queue")
+ * const serial = new SerialReqManager(115200, 0, "Stack")
+ * ```
+ */
+export class SerialReqManager extends SerialPVI {
    constructor(baud, parity, policy = "Queue") {
       super(baud, parity)
 
+      /**Permite pausar ou prosseguir com o processamento das requisições. ⚠️ Não impede a inserção de requisições no buffer.*/
       this.Processing = true
+      /**Buffer de requisições: é possível monitorar todas as informações de cada requisição inserida. */
       this.ReqBuffer = []
+      /**Buffer de respostas: é possível monitorar todas as informações de cada resposta inserida. ⚠️ Após atendida, a resposta é removida do buffer*/
       this.ResBuffer = []
-
+      /**Determina a politica de gerenciamento das requisições entre `fila` ou `pilha`*/
       this.policyManagement = policy
-      this.ManagerInterval = 200
+      /**Determina o intervalo em que as requisições serão processadas. */
+      this.ManagerInterval = 50
 
       this.Manager()
    }
@@ -161,8 +167,9 @@ class SerialReqManager extends SerialPVI {
          const nextReq = this.GetReq()
          const result = await this.reqResMatchBytes(nextReq)
 
-         nextReq["response"] = result.response
-         nextReq["sucess"] = result.sucess
+         nextReq["matchResult"] = result.response
+         nextReq["response"] = result.response[0]
+         nextReq["success"] = result.success
 
          this.ResBuffer.push(nextReq)
          this.Manager()
@@ -182,7 +189,7 @@ class SerialReqManager extends SerialPVI {
     * 
     * @returns requisicao: object
     * 
-    * Baseado na politica de gerenciamento: pilha, ou fila
+    * Baseado na politica de gerenciamento: "Stack", ou "Queue"
     */
    GetReq() {
       if (this.policyManagement == "Queue") {
@@ -199,10 +206,10 @@ class SerialReqManager extends SerialPVI {
    /**
     * Insere requisicao no buffer de requisicoes
     * @param {object} reqInfo
-    * @returns UniqueID: string
+    * @returns String
     */
    InsertReq(reqInfo) {
-      reqInfo.readTimeout ||= 100
+      reqInfo.readTimeout ||= 50
       reqInfo.tryNumber ||= 1
       reqInfo.maxTries ||= 3
       reqInfo["id"] = crypto.randomUUID()
@@ -213,7 +220,7 @@ class SerialReqManager extends SerialPVI {
    /**
     * Busca e remove do buffer de respostas uma resposta baseado no UUID
     * @param {string} searchID 
-    * @returns resposta: object
+    * @returns Object
     */
    SearchRes(searchID) {
       let obj = null
@@ -228,6 +235,40 @@ class SerialReqManager extends SerialPVI {
       return obj
    }
 
+
+   /**
+    * Insere um objeto na pilha ou fila do gerenciador, e retorno um objeto com as adquiridas.
+    * 
+    * @param {Object} reqInfo 
+    * @returns Object
+    * 
+    * # Exemplos
+    * 
+    * ```js
+    * const reqInfo = {
+    * request: String
+    * regex: RegExp
+    * readTimeout: Number //Opcional
+    * tryNumber: Number //Opcional
+    * maxTries: Number //Opcional
+    * }
+    * const serial = new SerialReqMenager(9600)
+    * const result = await serial.WatchForResponse(reqInfo)
+    * ```
+    * ## Result
+    * 
+    * ```js
+    * {id: String,
+    *  maxTries: Number,
+    *  readTimeout: Number,
+    *  regex: RegExp,
+    *  request: String,
+    *  matchResult: Array,
+    *  response: String,
+    *  success: Boolean,
+    *  tryNumber: Number}
+    * ```
+    */
    async WatchForResponse(reqInfo) {
       return new Promise((resolve) => {
          const id = this.InsertReq(reqInfo)
@@ -238,16 +279,16 @@ class SerialReqManager extends SerialPVI {
                clearInterval(monitor)
                resolve(obj)
             }
-         }, 100)
+         }, 50)
       })
    }
 
 }
 
-class SerialPVIUtil {
+export class SerialPVIUtil {
 
    static getPortList() {
-      return DAQ.runInstructionS("serial.getportscsv", []).split(";")
+      return FWLink.runInstructionS("serial.getportscsv", []).split(";")
    }
 
    static async closeAllPorts() {
@@ -255,7 +296,7 @@ class SerialPVIUtil {
 
          let ports = SerialPVIUtil.getPortList()
          for (const port of ports) {
-            console.log(`Fechando porta ${port}: `, pvi.runInstruction("serial.close", ['"' + port + '"']) == 1)
+            console.log(`Fechando porta ${port}: `, FWLink.runInstruction("serial.close", ['"' + port + '"']) == 1)
          }
          resolve()
       })
@@ -312,5 +353,10 @@ class SerialPVIUtil {
       } catch (error) {
          console.error("Erro ao converter hexadecimal para binario")
       }
+   }
+
+   static {
+      window.SerialPVIUtil = SerialPVIUtil
+      console.log("SerialPVIUtil is ready!")
    }
 }
